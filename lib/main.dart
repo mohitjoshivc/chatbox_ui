41358473c441ff1961ef4b6288fd70c146970479
Mohit Joshi <withmohitjoshi@gmail.com>
2024-11-13 22:12:50 +0530

    LandingPage done

---
+++ lib/main.dart
@@ -0,0 +1,37 @@
+import 'package:chatbox_ui/pages/landing_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Chatbox',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        fontFamily: 'Caros',
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ButtonStyle(
+            textStyle: WidgetStatePropertyAll(
+              TextStyle(
+                fontWeight: FontWeight.bold,
+                fontFamily: 'Caros',
+              ),
+            ),
+            shape: WidgetStatePropertyAll(
+              RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(16),
+              ),
+            ),
+          ),
+        ),
+      ),
+      home: LandingPage(),
+    );
+  }
+}
---
+++ lib/pages/landing_page.dart
@@ -0,0 +1,222 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class LandingPage extends StatelessWidget {
+  const LandingPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: LayoutBuilder(builder: (context, constraints) {
+        double screenWidth = constraints.maxWidth;
+        double screenHeight = constraints.maxHeight;
+        return Stack(
+          children: [
+            Container(
+              decoration: BoxDecoration(
+                gradient: RadialGradient(
+                  center: Alignment(0.0, -0.75),
+                  radius: 0.6,
+                  colors: [
+                    Color(0xFF43116A),
+                    Color(0xFF0A1832),
+                  ],
+                  stops: [0.35, double.infinity],
+                  tileMode: TileMode.clamp,
+                ),
+              ),
+            ),
+            SafeArea(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 24),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Container(
+                      padding:
+                          EdgeInsets.symmetric(vertical: screenHeight * 0.045),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          SvgPicture.asset(
+                            'assets/images/logo_small_white.svg',
+                            width: 19,
+                          ),
+                          SizedBox(
+                            width: 8,
+                          ),
+                          Text(
+                            "Chatbox",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 14,
+                              fontWeight: FontWeight.w600,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    SizedBox(
+                      width: screenWidth > 350 ? 350 : double.infinity,
+                      child: Padding(
+                        padding: const EdgeInsets.only(right: 24),
+                        child: FittedBox(
+                          child: RichText(
+                            text: TextSpan(
+                                text: "Connect\nfriends\n",
+                                style: TextStyle(
+                                  letterSpacing: 0,
+                                  fontFamily: 'Caros',
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.w400,
+                                  fontSize: 400,
+                                ),
+                                children: [
+                                  TextSpan(
+                                    text: 'easily &\nQuickly',
+                                    style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  )
+                                ]),
+                          ),
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: screenHeight * 0.02),
+                    Text(
+                      "Our chat app is the perfect way to stay connected with friends and family.",
+                      style: TextStyle(
+                        fontFamily: 'Circular',
+                        color: Color(0xFFB9C1BE),
+                        fontSize: 16,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                    SizedBox(height: screenHeight * 0.04),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Container(
+                          width: 48,
+                          height: 48,
+                          padding: EdgeInsets.all(8),
+                          decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            border: Border.all(
+                              color: Color(0xFFA8B0AF),
+                              width: 2.0,
+                            ),
+                          ),
+                          child: SvgPicture.asset(
+                              'assets/images/facebook_logo.svg'),
+                        ),
+                        SizedBox(width: 30),
+                        Container(
+                          width: 48,
+                          height: 48,
+                          padding: EdgeInsets.all(8),
+                          decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            border: Border.all(
+                              color: Color(0xFFA8B0AF),
+                              width: 2.0,
+                            ),
+                          ),
+                          child:
+                              SvgPicture.asset('assets/images/google_logo.svg'),
+                        ),
+                        SizedBox(width: 30),
+                        Container(
+                          width: 48,
+                          height: 48,
+                          padding: EdgeInsets.all(8),
+                          decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            border: Border.all(
+                              color: Color(0xFFA8B0AF),
+                              width: 2.0,
+                            ),
+                          ),
+                          child:
+                              SvgPicture.asset('assets/images/apple_logo.svg'),
+                        ),
+                      ],
+                    ),
+                    SizedBox(height: screenHeight * 0.03),
+                    Spacer(
+                      flex: 2,
+                    ),
+                    Row(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Expanded(
+                          child: Divider(
+                            color: Color(0xFFCDD1D0),
+                          ),
+                        ),
+                        SizedBox(width: 14),
+                        Text(
+                          "OR",
+                          style: TextStyle(
+                            color: Color(0xFFD6E4E0),
+                            fontFamily: 'Circular',
+                            fontSize: 14,
+                          ),
+                        ),
+                        SizedBox(width: 14),
+                        Expanded(
+                          child: Divider(
+                            color: Color(0xFFCDD1D0),
+                          ),
+                        ),
+                      ],
+                    ),
+                    SizedBox(height: screenHeight * 0.03),
+                    ElevatedButton(
+                      style: ButtonStyle(
+                          minimumSize: WidgetStatePropertyAll(
+                            Size.fromHeight(48),
+                          ),
+                          backgroundColor:
+                              WidgetStatePropertyAll(Colors.white)),
+                      onPressed: () {},
+                      child: Text(
+                        "Sign up with mail",
+                        style: TextStyle(color: Colors.black),
+                      ),
+                    ),
+                    SizedBox(height: screenHeight * 0.05),
+                    Align(
+                      alignment: Alignment.center,
+                      child: RichText(
+                        text: TextSpan(
+                            text: "Existing account? ",
+                            style: TextStyle(
+                              fontFamily: 'Circular',
+                              color: Color(0xFFB9C1BE),
+                              fontSize: 16,
+                              fontWeight: FontWeight.w600,
+                            ),
+                            children: [
+                              TextSpan(
+                                text: "Log in",
+                                style: TextStyle(
+                                  color: Colors.white,
+                                ),
+                              )
+                            ]),
+                      ),
+                    ),
+                    Spacer(flex: 1),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        );
+      }),
+    );
+  }
+}
